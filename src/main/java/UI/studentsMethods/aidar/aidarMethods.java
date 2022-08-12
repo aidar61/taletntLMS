43a3fc1efa40c7a956dbe6a9129d55f30f497dfd
Aidar Askeev <aidarka61@Aidars-MacBook-Pro.local>
2022-08-12 19:53:01 +0600

    commit

---
+++ src/main/java/UI/studentsMethods/aidar/aidarMethods.java
@@ -0,0 +1,7 @@
+package UI.studentsMethods.aidar;
+
+public class aidarMethods {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
---
+++ src/main/java/UI/studentsMethods/asan/asanMethods.java
@@ -0,0 +1,7 @@
+package UI.studentsMethods.asan;
+
+public class asanMethods {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
