364c14254e01efa632775b6b3cd8b7ee3e832824
Aidar Askeev <aidarka61@Aidars-MacBook-Pro.local>
2022-08-12 20:12:21 +0600

    commit

---
+++ src/main/java/UI/testPage/addUserTestAidar/addUser.java
@@ -0,0 +1,7 @@
+package UI.testPage.addUserTestAidar;
+
+public class addUser {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
---
+++ src/main/java/UI/testPage/usersTestAsan/User.java
@@ -0,0 +1,7 @@
+package UI.testPage.usersTestAsan;
+
+public class User {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
